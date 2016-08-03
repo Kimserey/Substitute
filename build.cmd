c9bc6356f5f212807fc548c3ca3771e0d28d58d1
Kimserey <kimserey.lam@housing-insight.co.uk>
2016-08-03 16:02:27 +0100

    added build script

---
+++ build.cmd
@@ -0,0 +1,14 @@
+"C:\Projects\Substitute\packages\WebSharper.3.6.14.237\tools\net40\WebSharper.exe"^
+ -r "C:\Projects\Substitute\packages\WebSharper.3.6.14.237\lib\net40\WebSharper.Core.JavaScript.dll"^
+ -r "C:\Projects\Substitute\packages\WebSharper.3.6.14.237\lib\net40\WebSharper.Collections.dll"^
+ -r "C:\Projects\Substitute\packages\WebSharper.3.6.14.237\lib\net40\WebSharper.Core.dll"^
+ -r "C:\Projects\Substitute\packages\WebSharper.3.6.14.237\lib\net40\WebSharper.JavaScript.dll"^
+ -r "C:\Projects\Substitute\packages\WebSharper.3.6.14.237\lib\net40\WebSharper.JQuery.dll"^
+ -r "C:\Projects\Substitute\packages\WebSharper.3.6.14.237\lib\net40\WebSharper.Main.dll"^
+ -r "C:\Projects\Substitute\packages\WebSharper.3.6.14.237\lib\net40\WebSharper.Sitelets.dll"^
+ -r "C:\Projects\Substitute\packages\WebSharper.3.6.14.237\lib\net40\WebSharper.Web.dll"^
+ -r "C:\Projects\Substitute\packages\WebSharper.UI.Next.3.6.15.211\lib\net40\WebSharper.UI.Next.dll"^
+ -r "C:\Projects\Substitute\packages\WebSharper.UI.Next.3.6.15.211\lib\net40\WebSharper.UI.Next.Templating.dll"^
+ -r "C:\Projects\Substitute\Substitute.Two\bin\Debug\Substitute.Two.dll"^
+ "C:\Projects\Substitute\Substitute\bin\Substitute.dll"^
+ "C:\Projects\Substitute\Substitute\bin\Substitute_cp.dll"
